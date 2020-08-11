dcdf3f9f8f4c18be265163f56803c6149b4c4b16
ibrag8998 <ibragdzh@gmail.com>
2020-08-11 08:08:32 +0300

    separate module for text snippets

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/actions.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/actions.py
@@ -2,7 +2,9 @@ import typing as typ
 
 from chaban.actions import Action
 
+from . import text
+
 
 class StartCommandAction(Action):
     def act(self, message: typ.Dict[str, typ.Any]) -> None:
-        self.tbot.send_message(message["chat"]["id"], "Welcome!")
+        self.tbot.send_message(message["chat"]["id"], text.START_COMMAND)
---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/text.py
@@ -0,0 +1,2 @@
+START_COMMAND = 'Welcome to the {{ cookiecutter.project_name }}!'
+

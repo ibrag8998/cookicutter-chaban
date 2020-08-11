f47edf718a1cdda43639ade6e0dc6f4b098affeb
ibrag8998 <ibragdzh@gmail.com>
2020-08-11 06:32:45 +0300

    post gen hook to rename gitignore

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,8 @@
 import os
 
-print(os.getcwd())
-print(os.path.abspath('{{ cookiecutter.project_slug }}'))
+# get cwd
+cwd = os.getcwd()
+
+# rename "gitignore" to ".gitignore"
+gitignore_file = os.path.join(cwd, "gitignore")
+os.rename(gitignore_file, "." + gitignore_file)

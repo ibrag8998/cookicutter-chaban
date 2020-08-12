1cb28e37150fa8d88220e18e2853c3efcc7d18af
ibrag8998 <ibragdzh@gmail.com>
2020-08-12 22:21:27 +0300

    hotfix in mkenv.sh script

--- {{ cookiecutter.project_slug }}/scripts/mkenv.sh
+++ {{ cookiecutter.project_slug }}/scripts/mkenv.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 cd `dirname $(realpath $0)`
 
-read -p "TELEGRAM_TOKEN: " $TELEGRAM_TOKEN
+read -p "TELEGRAM_TOKEN: " TELEGRAM_TOKEN
 
-read -p "DEBUG [Y/n]: " $debug_yn
-$DEBUG=True
+read -p "DEBUG [Y/n]: " debug_yn
+DEBUG=True
 if [[ "$debug_yn" == 'n' ]]; then
-    $DEBUG=False
+    DEBUG=False
 fi
 
 
 code="\
 TELEGRAM_TOKEN=\"$TELEGRAM_TOKEN\"
-DEBUG=$DEBUG
-"
+DEBUG=$DEBUG"
 echo "$code" > ../.env

fd5f84144a47e7e4f4fa078de433fd0d0143dc42
ibrag8998 <ibragdzh@gmail.com>
2020-08-12 22:31:30 +0300

    installdeps.sh script fixes and add default value (base)

--- {{ cookiecutter.project_slug }}/scripts/installdeps.sh
+++ {{ cookiecutter.project_slug }}/scripts/installdeps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd `dirname $(realpath $0)`
 
-. ../{{ cookiecutter.venv_path }}/bin/pip install -r requirements/$1.txt
+req_type = ${1:-base}
+
+. ../{{ cookiecutter.venv_path }}/bin/pip install -r ../requirements/$1.txt

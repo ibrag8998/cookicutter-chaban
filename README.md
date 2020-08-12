8ed2bc4e1e599880e820d9beff1fb0f39b258ade
ibrag8998 <ibragdzh@gmail.com>
2020-08-12 22:28:01 +0300

    install requirements not on project init, but when user wants

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ cookiecutter gh:ibrag8998/cookiecutter-chaban
 # ...
 cd <project_slug>
 . venv/bin/activate  # activate venv
+./scripts/installdeps.sh
+./scripts/mkenv.sh
 ./run.py
 ```
--- hooks/post_gen_project.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print(" ~~ Creating virtual environment ...")
-os.system("python3 -m venv {{ cookiecutter.venv_path }}")
-print(" == Done")
-
-print(" ~~ Installing local python requirements ...")
-os.system("./venv/bin/pip install -r requirements/local.txt > /dev/null")
-print(" == Done")
